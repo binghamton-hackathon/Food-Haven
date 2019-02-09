50241699bf1ba53b433fbd95c3221956da9b78a5
David Jiang <djiang86@binghamton.edu>
2019-02-09 21:56:38 +0000

    foce push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { AboutPanelComponent } from './components/about-panel/about-panel.component';
 import { LargePanelComponent } from './components/large-panel/large-panel.component';
+import { SmallButtonComponent } from './components/buttons/small-button/small-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AboutPanelComponent,
-    LargePanelComponent
+    LargePanelComponent,
+    SmallButtonComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/buttons/small-button/small-button.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SmallButtonComponent } from './small-button.component';
+
+describe('SmallButtonComponent', () => {
+  let component: SmallButtonComponent;
+  let fixture: ComponentFixture<SmallButtonComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SmallButtonComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SmallButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
